6c9eb1471d22f7e407d001d55d906d9603a088aa
AndreaGuarracino <andreaguarracino@outlook.com>
2021-11-13 11:24:16 +0100

    evaluate also different window sizes

--- scripts/45_fish/45_fish.sh
+++ scripts/45_fish/45_fish.sh
@@ -1,4 +1,4 @@
-num_threads=16
+num_threads=48
 
 mkdir -p /lizardfs/guarracino/vgp/45_fish
 cd /lizardfs/guarracino/vgp/45_fish
@@ -40,7 +40,9 @@ done
 cd /lizardfs/guarracino/vgp/45_fish
 
 
-# Align and evaluate
+# Mapping and evaluate
+mkdir /lizardfs/guarracino/vgp/45_fish/mappings
+
 query=GCA_904848185.1_fAcaLat1.1
 target=GCA_900880675.2_fSpaAur1.2
 
@@ -51,28 +53,31 @@ path_complete_busco_genes_target=busco_genes/"$target"/$target.busco_genes.compl
 total_genes_in_query=$(cat $path_complete_busco_genes_query | wc -l)
 total_genes_in_target=$(cat $path_complete_busco_genes_target | wc -l)
 
-echo query target total_genes_in_query total_genes_in_target s l p present_gene_ratio_query present_gene_ratio_target missing_genes_in_query missing_genes_in_target | tr ' ' '\t' > search.tsv
-for s in 50k 100k 150k 200k 250k 300k 350k 400k 450k 500k 600k 700k 800k 900k 1M
+echo query target total_genes_in_query total_genes_in_target s l p w present_gene_ratio_query present_gene_ratio_target missing_genes_in_query missing_genes_in_target | tr ' ' '\t' > busco_evaluation.mapping.tsv
+for s in 20k 50k 100k 150k 200k 250k 300k 350k 400k 450k 500k 600k 700k 800k 900k 1M
 do
   l=$s
   for p in 85 80 75 70
   do
-    prefix=$query-$target.s$s.l$l.p$p
+    for w in 256 0
+    do
+      prefix=$query-$target.s$s.l$l.p$p.w$w
 
-    if [ ! -f $prefix.approx.paf ]; then
-      wfmash genomes/${target}_genomic.fna.gz genomes/${query}_genomic.fna.gz -t $num_threads -s $s -l $l -p $p -m >$prefix.approx.paf
-    fi
+      if [ ! -f $prefix.approx.paf ]; then
+        wfmash genomes/${target}_genomic.fna.gz genomes/${query}_genomic.fna.gz -t $num_threads -s $s -l $l -p $p -w $w -m >mappings/$prefix.approx.paf
+      fi
 
-    cat $prefix.approx.paf | awk -v OFS='\t' '{print $1, $3, $4, "", "", "+"}' >$prefix.approx.$query.bed
-    cat $prefix.approx.paf | awk -v OFS='\t' '{print $6, $8, $9, "", "", $5}' >$prefix.approx.$target.bed
+      cat mappings/$prefix.approx.paf | awk -v OFS='\t' '{print $1, $3, $4, "", "", "+"}' >mappings/$prefix.approx.$query.bed
+      cat mappings/$prefix.approx.paf | awk -v OFS='\t' '{print $6, $8, $9, "", "", $5}' >mappings/$prefix.approx.$target.bed
 
-    # Number of complete BUSCO genes not entirely covered
-    missing_genes_in_query=$(bedtools subtract -a $path_complete_busco_genes_query -b $prefix.approx.$query.bed | cut -f 4 | sort | uniq | wc -l)
-    missing_genes_in_target=$(bedtools subtract -a $path_complete_busco_genes_target -b $prefix.approx.$target.bed | cut -f 4 | sort | uniq | wc -l)
+      # Number of complete BUSCO genes not entirely covered
+      missing_genes_in_query=$(bedtools subtract -a $path_complete_busco_genes_query -b mappings/$prefix.approx.$query.bed | cut -f 4 | sort | uniq | wc -l)
+      missing_genes_in_target=$(bedtools subtract -a $path_complete_busco_genes_target -b mappings/$prefix.approx.$target.bed | cut -f 4 | sort | uniq | wc -l)
 
-    missing_gene_ratio_query=$(echo "scale=4; 1 - $missing_genes_in_query / $total_genes_in_query" | bc)
-    missing_gene_ratio_target=$(echo "scale=4; 1 - $missing_genes_in_target / $total_genes_in_target" | bc)
+      missing_gene_ratio_query=$(echo "scale=4; 1 - $missing_genes_in_query / $total_genes_in_query" | bc)
+      missing_gene_ratio_target=$(echo "scale=4; 1 - $missing_genes_in_target / $total_genes_in_target" | bc)
 
-    echo $query $target $total_genes_in_query $total_genes_in_target $s $l $p $missing_gene_ratio_query $missing_gene_ratio_target $missing_genes_in_query $missing_genes_in_target | tr ' ' '\t' >> search.tsv
+      echo $query $target $total_genes_in_query $total_genes_in_target $s $l $p $w $missing_gene_ratio_query $missing_gene_ratio_target $missing_genes_in_query $missing_genes_in_target | tr ' ' '\t' >> busco_evaluation.mapping.tsv
+    done
   done
 done
