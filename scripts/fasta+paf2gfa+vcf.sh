bcd45fb02e8f3862a6e0ff41bee9a1028bba4db6
AndreaGuarracino <andreaguarracino@outlook.com>
2022-08-02 11:30:09 +0200

    update scripts folder

---
+++ scripts/fasta+paf2gfa+vcf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PATH_SEQWISH=/home/guarracino/tools/seqwish/bin/seqwish-ccfefb016fcfc9937817ce61dc06bbcf382be75e
+PATH_VG=/home/guarracino/tools/vg
+
+# Example:
+# bash scripts/fasta+paf2vcf.sh data/LPA.subset.fa.gz 100k 300k 98 14 chm13 '_' ~/tools/wfmash/build/bin/wfmash-master master 48
+
+# Inputs
+FASTA=$1
+SEGMENT_LEN=$2
+BLOCK_LEN=$3
+IDENTITY=$4
+NUM_HAPLOTYPES=$5
+REFERENCE_PREFIX=$6
+SEPARATOR=$7
+PATH_WFMASH=$8
+PREFIX=$9
+THREADS=${10}
+
+FASTA_PREFIX=$(basename "$FASTA" .fa.gz)
+
+# Paths
+PAF=${PREFIX}.${FASTA_PREFIX}.s${SEGMENT_LEN}.l${BLOCK_LEN}.p$IDENTITY.n${NUM_HAPLOTYPES}.paf
+GFA=$PAF.k0.gfa
+VCF=$GFA.vcf.gz
+
+echo "All-vs-all alignment"
+\time -v $PATH_WFMASH "$FASTA" "$FASTA" -X -s "$SEGMENT_LEN" -l "$BLOCK_LEN" -p "$IDENTITY" -n "$NUM_HAPLOTYPES" -t "$THREADS" > "$PAF"
+
+echo "Graph induction"
+# -k 0` is for getting a lossless representation of the pairwise alignment
+\time -v ${PATH_SEQWISH} -s "$FASTA" -p "$PAF" -g "$GFA" -k 0 -B 10M -t "$THREADS" -P
+
+echo "Identify variants"
+\time -v ${PATH_VG} deconstruct -e -a -P "$REFERENCE_PREFIX" -H "$SEPARATOR" "$GFA" -t "$THREADS" | bgzip -c > "$VCF" && tabix "$VCF"
---
+++ scripts/mini_tomato/README.md
@@ -0,0 +1,8 @@
+# usage
+
+```shell
+PATH_WFMASH=/home/guarracino/tools/wfmash/build/bin/wfmash-ad8aebae1be96847839778af534866bc9545adb9
+
+
+$PATH_WFMASH 
+```
