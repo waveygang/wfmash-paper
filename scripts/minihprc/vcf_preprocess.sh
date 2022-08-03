17c26863d9e410856a43c0b5ca4ef99d7944b8b5
AndreaGuarracino <andreaguarracino@outlook.com>
2022-08-03 10:04:39 +0200

    generalize vcf_preprocess.sh

--- scripts/minihprc/vcf_preprocess.sh
+++ scripts/minihprc/vcf_preprocess.sh
@@ -6,15 +6,16 @@
 VCF=$1
 SAMPLE=$2
 REF=$3
-MAXSIZE=$4
+CHROMS=$4
+MAXSIZE=$5
 
 FNAME=$(basename $VCF)
 PREFIX=$(dirname $VCF)/"${FNAME%.vcf.gz}"
-CHROMS="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY"
+
 MEM="10G"
 
 bcftools view -a -s ${SAMPLE} -Ou ${VCF} \
-    | bcftools norm -f ${REF} -c s -m - -Ou \
+    | bcftools norm q-f ${REF} -c s -m - -Ou \
     | bcftools view -e 'GT="ref" | GT~"\."' -f 'PASS,.' -Ou \
     | bcftools sort -m ${MEM} -T bcftools-sort.XXXXXX -Ou \
     | bcftools norm -d exact -Oz -o ${PREFIX}.norm.vcf.gz \
