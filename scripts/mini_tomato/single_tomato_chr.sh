a65ef4da7cd857612637d0c3c77a04b521b1f6d7
AndreaGuarracino <andreaguarracino@outlook.com>
2022-08-04 22:29:50 +0200

    fix `sed`; enable waving

--- scripts/mini_tomato/single_tomato_chr.sh
+++ scripts/mini_tomato/single_tomato_chr.sh
@@ -38,12 +38,12 @@ PATH_VCF_GZ=$CHR.s$s.l$l.p$p.n$n.k0.vcf.gz
 
 # Fix CHROM (to match the CHROM in the truth variant set)
 mv $PATH_VCF_GZ xxx.vcf.gz
-zcat xxx.vcf.gz | sed 's/SL5#1#ch//g' | sed 's/>0/>/g' | bgzip -c -@ 48 > $PATH_VCF_GZ
+zcat xxx.vcf.gz | sed 's/SL5#1#ch//g' | sed 's/^0//g' | bgzip -c -@ 48 > $PATH_VCF_GZ
 rm xxx.vcf.gz
 
 #echo "Realign REF/ALT alleles"
-#PATH_VCF_WAVED_GZ=$CHR.s$s.l$l.p$p.n$n.k0.waved.vcf.gz
-#vcfbub -l 0 -a 100000 --input $PATH_VCF_GZ | $vcfwave -I 1000 -t 48 | bgzip -c -@ 48 > $PATH_VCF_WAVED_GZ
+PATH_VCF_WAVED_GZ=$CHR.s$s.l$l.p$p.n$n.k0.waved.vcf.gz
+vcfbub -l 0 -a 100000 --input $PATH_VCF_GZ | $vcfwave -I 1000 -t 48 | bgzip -c -@ 48 > $PATH_VCF_WAVED_GZ
 
 # Compare query/truth
 for SAMPLE in PP TS204 TS281 TS413 TS629 TS96; do
@@ -71,19 +71,19 @@ for SAMPLE in PP TS204 TS281 TS413 TS629 TS96; do
     -T 48 \
     -o vcfeval/$SAMPLE
 
-#  bash /lizardfs/guarracino/wfmash-paper/scripts/vcf_preprocess.sh \
-#    $PATH_VCF_WAVED_GZ \
-#    $SAMPLE \
-#    $PATH_REF_FASTA \
-#    $i \
-#    50
-#  QUERY_WAVED_VCF_GZ=$CHR.s$s.l$l.p$p.n$n.k0.waved.norm.max50.vcf.gz
-#  rtg vcfeval \
-#    -t $PATH_REF_SDF \
-#    -b $TRUTH_VCF_GZ \
-#    -c $QUERY_WAVED_VCF_GZ \
-#    -T 48 \
-#    -o vcfeval/$SAMPLE.waved
+  bash /lizardfs/guarracino/wfmash-paper/scripts/vcf_preprocess.sh \
+    $PATH_VCF_WAVED_GZ \
+    $SAMPLE \
+    $PATH_REF_FASTA \
+    $i \
+    50
+  QUERY_WAVED_VCF_GZ=$CHR.s$s.l$l.p$p.n$n.k0.waved.norm.max50.vcf.gz
+  rtg vcfeval \
+    -t $PATH_REF_SDF \
+    -b $TRUTH_VCF_GZ \
+    -c $QUERY_WAVED_VCF_GZ \
+    -T 48 \
+    -o vcfeval/$SAMPLE.waved
 done
 
 DIR_OUTPUT_PREFIX=$DIR_OUTPUT/$PREFIX
