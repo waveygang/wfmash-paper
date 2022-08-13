b5ed1bba5219684796e569590946fa0e88ea1d27
AndreaGuarracino <andreaguarracino@outlook.com>
2022-08-13 14:11:19 +0200

    nucmer-based evaluation for tomato

---
+++ scripts/mini_tomato/single_tomato_chr_nucmer.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Input
+s=$1
+l=$2
+p=$3
+n=$4
+i=$5 # Num. chromosome
+PATH_WFMASH=$6
+PREFIX=$7
+DIR_OUTPUT=$8
+
+# Paths
+DIR_GENOMES=/lizardfs/guarracino/wfmash-paper/tomato/genomes
+
+
+CHR=chr$i
+
+rm -rf /scratch/$CHR
+mkdir -p /scratch/$CHR
+cd /scratch/$CHR
+
+PATH_FASTA_GZ=${DIR_GENOMES}/chr$i.fa.gz
+
+bash /lizardfs/guarracino/wfmash-paper/scripts/fasta+paf2gfa+vcf.sh \
+  $PATH_FASTA_GZ \
+  $s $l $p $n \
+  SL5 '#' \
+  "$PATH_WFMASH" \
+  $CHR \
+  48 | tee fasta+paf2gfa+vcf.log
+
+PATH_GFA=$CHR.s$s.l$l.p$p.n$n.k0.gfa
+
+# Compare query/truth
+bash /lizardfs/guarracino/pggb-paper/scripts/gfa2evaluation.sh $PATH_GFA SL5 evaluation 48
+
+DIR_OUTPUT_PREFIX=$DIR_OUTPUT/$PREFIX
+mkdir -p $DIR_OUTPUT_PREFIX
+
+cd /scratch/
+mv /scratch/$CHR $DIR_OUTPUT_PREFIX
